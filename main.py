1c029d12364f64f9a14561ee564facdf516dd00b
ilyinily <ilyinily@gmail.com>
2023-11-08 00:12:27 +0300

    add / root reaction

--- main.py
+++ main.py
@@ -23,13 +23,13 @@ class Timestamp(BaseModel):
 
 
 dogs_db = {
-    0: Dog(name='Bob', pk=0, kind='terrier'),
-    1: Dog(name='Marli', pk=1, kind="bulldog"),
-    2: Dog(name='Snoopy', pk=2, kind='dalmatian'),
-    3: Dog(name='Rex', pk=3, kind='dalmatian'),
-    4: Dog(name='Pongo', pk=4, kind='dalmatian'),
-    5: Dog(name='Tillman', pk=5, kind='bulldog'),
-    6: Dog(name='Uga', pk=6, kind='bulldog')
+    0: Dog(name="Bob", pk=0, kind="terrier"),
+    1: Dog(name="Marli", pk=1, kind="bulldog"),
+    2: Dog(name="Snoopy", pk=2, kind="dalmatian"),
+    3: Dog(name="Rex", pk=3, kind="dalmatian"),
+    4: Dog(name="Pongo", pk=4, kind="dalmatian"),
+    5: Dog(name="Tillman", pk=5, kind="bulldog"),
+    6: Dog(name="Uga", pk=6, kind="bulldog")
 }
 
 post_db = [
@@ -38,10 +38,9 @@ post_db = [
 ]
 
 
-@app.get('/')
+@app.get("/")
 def root():
-    # ваш код здесь
-    ...
+    return ("Вас приветствует ветеринарная клиника имени Тим. Собакина. Используйте команды из документации."
+            "Для просмотра документации используйте ключ docs.")
 
 # ваш код здесь
-...
